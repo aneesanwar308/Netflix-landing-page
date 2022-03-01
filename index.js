c14d14a3daa0b73f3980810acdeef8d38c0d88e6
aneesanwar308 <anwaranees250@gmail.com>
2022-03-01 12:27:33 +0500

    Netflix landing page designed using HTML, CSS & Javascript

---
+++ index.js
@@ -0,0 +1,18 @@
+const tabItems = document.querySelectorAll(".tab-item")
+const tabContentItems = document.querySelectorAll(".tab-content-item")
+
+function selectItem(e) {
+    removeBorder()
+    removeShowClass()
+    this.classList.add('tab-border')
+    const tabContentItem = document.querySelector(`#${this.id}-content`)
+    tabContentItem.classList.add("show")
+}
+function removeBorder() {
+    tabItems.forEach(item => item.classList.remove('tab-border'))
+}
+function removeShowClass() {
+    tabContentItems.forEach(item => item.classList.remove("show"))
+}
+
+tabItems.forEach(item => item.addEventListener("click", selectItem))
